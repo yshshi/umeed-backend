123ea05e699a76eeeee610d87e481517f16e9aa6
Yash <yashkantsingh3@gmail.com>
2026-03-01 13:22:56 +0530

    updated cors

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ const registrationRoutes = require('./routes/registrationRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 
 const app = express();
-app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:5173', credentials: true }));
+// app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin:'https://umeed-ngo.com/', credentials: true }));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
